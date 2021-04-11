8c27e2cd3e528dc6712d89666a83472fa0dfa032
ernesto_sanchez_matos <u201713827@upc.edu.pe>
2021-04-11 00:36:46 -0500

    --

---
+++ api_rekognition.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 31 15:34:01 2021
+
+@author: Ryzen
+"""
+import os
+import csv
+import boto3
+import io
+
+from botocore.config import Config
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage    
+import json
+
+app=Flask(__name__)
+app.config['UPLOAD_FOLDER']="./Imagenes"
+
+def prediccion(f):    
+    #CONFIGURAR LOS ACCESOS
+     client=boto3.client('rekognition',
+                    region_name='us-east-2',
+                    aws_access_key_id="AKIA2GIV3UNTGU3P23WU",
+                    aws_secret_access_key="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"
+                    )     
+     filename=secure_filename(f.filename)
+     f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))    
+     #Uso de AWS para la deteccion
+     response=client.detect_faces(
+         Image={'Bytes':f.getvalue()},  
+         Attributes=['ALL']) 
+     
+     return response
+
+
+@app.route("/")
+def pagina_principal():   
+    return render_template('formulario.html')
+
+@app.route("/Predictor",methods=['POST'])
+def Predictor():    
+    if request.method=='POST':
+        f=request.files['archivo']        
+        response=prediccion(f)
+        #MOSTRAR LA RESPUESTA
+        return {
+            'statusCode': 200,
+            'body': response        
+        }      
+    
+if __name__=='__main__':
+    app.run(debug=True)
+            
