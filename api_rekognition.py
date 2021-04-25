ecfe067222d2fd5f9f561a782400905ef499a39f
ernesto_sanchez_matos <u201713827@upc.edu.pe>
2021-04-25 02:07:56 -0500

    __

--- api_rekognition.py
+++ api_rekognition.py
@@ -15,6 +15,7 @@ from botocore.config import Config
 from flask import Flask,render_template,request
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage    
+from entrenandoRF import GenerarModelo
 import json
 
 app=Flask(__name__)
@@ -98,7 +99,8 @@ def prediccion(f):
                   
       return _response
  
-def RegisterFaceUser(dni,f):     
+def RegisterFaceUser(dni,f):    
+    
     dataPath = app.config['UPLOAD_FOLDER_IMAGENES']    
     personPath = dataPath + '/' + dni     
    
@@ -109,32 +111,34 @@ def RegisterFaceUser(dni,f):
     cap = cv2.VideoCapture(videoPersonaPath)          
     
     if not os.path.exists(personPath):	
-    	os.makedirs(personPath)
+     	os.makedirs(personPath)
     
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
     count = 0
     
     while True:
+            
+     	ret, frame = cap.read()
+     	if ret == False: break
+     	frame =  imutils.resize(frame, width=640)
+     	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+     	auxFrame = frame.copy()
     
-    	ret, frame = cap.read()
-    	if ret == False: break
-    	frame =  imutils.resize(frame, width=640)
-    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    	auxFrame = frame.copy()
+     	faces = faceClassif.detectMultiScale(gray,1.3,5)
     
-    	faces = faceClassif.detectMultiScale(gray,1.3,5)
+     	for (x,y,w,h) in faces:             
+             cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+             rostro = auxFrame[y:y+h,x:x+w]
+             rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+             cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count),rostro)
+             count = count + 1
+     	#cv2.imshow('frame',frame)
     
-    	for (x,y,w,h) in faces:
-    		cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-    		rostro = auxFrame[y:y+h,x:x+w]
-    		rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
-    		cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count),rostro)
-    		count = count + 1
-    	#cv2.imshow('frame',frame)
+     	k =  cv2.waitKey(1)
+     	if k == 27 or count >= 300:
+             break
     
-    	k =  cv2.waitKey(1)
-    	if k == 27 or count >= 300:
-    		break
+    GenerarModelo()
 
     
 
@@ -178,5 +182,5 @@ def Cargardata():
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='192.168.1.123',port=8123,debug=True)
             
