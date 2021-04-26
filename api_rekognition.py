044d078437211fae7ffc44b29ff8dd1a7e4ef28f
ernesto_sanchez_matos <u201713827@upc.edu.pe>
2021-04-26 18:14:54 -0500

    Update api_rekognition.py

--- api_rekognition.py
+++ api_rekognition.py
@@ -22,8 +22,7 @@ app=Flask(__name__)
 app.config['UPLOAD_FOLDER_IMAGENES']="./Imagenes"
 app.config['UPLOAD_FOLDER_IMAGENES_RQST']="./ImagenesRQST"
 app.config['UPLOAD_FOLDER_VIDEOS']="./VideosData"
-
-área="simple"                
+             
 
 def Reconocimiento(f): 
     imagePaths = os.listdir(app.config['UPLOAD_FOLDER_IMAGENES'])
