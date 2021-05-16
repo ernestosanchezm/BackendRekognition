6124aa23567b32417b03e48183021d6865aa44ce
ernesto_sanchez_matos <u201713827@upc.edu.pe>
2021-05-16 09:18:11 -0500

    registro_dataset

--- api_rekognition.py
+++ api_rekognition.py
@@ -17,13 +17,19 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage    
 from entrenandoRF import GenerarModelo
 import json
+import uuid
 
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER_IMAGENES']="./Imagenes"
 app.config['UPLOAD_FOLDER_IMAGENES_RQST']="./ImagenesRQST"
 app.config['UPLOAD_FOLDER_VIDEOS']="./VideosData"
-    
-         
+app.config['UPLOAD_FOLDER_VIDEOS_DATASET']="./VideosDataSet"
+                
+configBD={ 
+    'host': "db-proyecto-mysql.cluster-cbwzye3glmch.us-east-1.rds.amazonaws.com",
+    'user' : "sa",
+    'passwd' : "luismiguel",
+    'database' : "educati"}
 
 def Reconocimiento(f): 
     imagePaths = os.listdir(app.config['UPLOAD_FOLDER_IMAGENES'])
@@ -107,8 +113,7 @@ def RegisterFaceUser(dni,f):
     faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
     count = 0
     
-    while True:
-            
+    while True:            
      	ret, frame = cap.read()
      	if ret == False: break
      	frame =  imutils.resize(frame, width=640)
@@ -131,19 +136,123 @@ def RegisterFaceUser(dni,f):
     
     GenerarModelo()
     return "Registro exitoso "+objUser['nombre']
+
+
+def RegistarDataset(f):          
+    nombreVideo=str(uuid.uuid4())
+    dataPath = app.config['UPLOAD_FOLDER_IMAGENES']   
+    f.save(os.path.join(app.config['UPLOAD_FOLDER_VIDEOS_DATASET'],nombreVideo+".mp4"))  
     
-def GetUserForToken(token):
-    #CONECCION A MYSQL
-    db = mysql.connect(
-        host = "db-proyecto-mysql.cluster-cbwzye3glmch.us-east-1.rds.amazonaws.com",
-        user = "sa",
-        passwd = "luismiguel",
-        database = "educati"
-        )
+    #luego de guardar el video, usarlo de captura
+    videoPersonaPath=app.config['UPLOAD_FOLDER_VIDEOS_DATASET']+"/"+nombreVideo+".mp4"
+    cap = cv2.VideoCapture(videoPersonaPath)          
+    
+    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+    count = 0
+    respuestas=[]
+    
+    #CONFIGURAR LOS ACCESOS
+    client=boto3.client('rekognition',
+                          region_name='us-east-2',
+                          aws_access_key_id="AKIA6P46JAOPIR5NGNNC",
+                          aws_secret_access_key="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"
+                          )  
+    cantCONFUSED=0
+    cantANGRY=0
+    cantDISGUSTED=0
+    cantSAD=0
+    cantCALM=0
+    cantSURPRISED=0
+    cantFEAR=0
+    cantHAPPY=0
+    
+    mayor_confidence=0
+    while True:
+            
+     	ret, frame = cap.read()
+     	if ret == False: break
+     	frame =  imutils.resize(frame, width=640)
+     	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+     	auxFrame = frame.copy()
+         
+     	faces = faceClassif.detectMultiScale(gray,1.3,5)
+    
+     	for (x,y,w,h) in faces:             
+              cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+              rostro = auxFrame[y:y+h,x:x+w]
+              rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+              cv2.imshow('image', rostro)
+              imagen=cv2.imencode('.jpg', rostro)[1].tostring()                      
+              #Uso de AWS para la deteccion
+              response=client.detect_faces(
+                 Image={'Bytes':imagen},  
+                 Attributes=['ALL'])    
+                          
+             
+
+              _response=''
+              try:        
+                   for element in response["FaceDetails"][0]["Emotions"]:
+                       if element["Confidence"]>mayor_confidence:
+                           mayor_confidence=element["Confidence"]   
+                        
+                   for element in response["FaceDetails"][0]["Emotions"]:
+                       if mayor_confidence==element["Confidence"]:
+                           if element["Type"]=='CONFUSED':
+                               cantCONFUSED+=1
+                           if element["Type"]=='ANGRY':
+                               cantANGRY+=1
+                           if element["Type"]=='DISGUSTED':
+                               cantDISGUSTED+=1                          
+                           if element["Type"]=='SAD':
+                               cantSAD+=1
+                           if element["Type"]=='CALM':
+                               cantCALM+=1
+                           if element["Type"]=='SURPRISED':
+                               cantSURPRISED+=1
+                           if element["Type"]=='FEAR':
+                               cantFEAR+=1
+                           if element["Type"]=='HAPPY':
+                               cantHAPPY+=1
+                               
+              except:
+                   print('error')
+              finally:
+                   count = count + 1                     
+              
+     	k =  cv2.waitKey(1)
+     	if k == 27 or count >=50:
+              break
+    total=cantCONFUSED+cantANGRY+cantDISGUSTED+cantSAD+cantCALM+cantSURPRISED+cantFEAR+cantHAPPY
+    pCONFUSED=round(cantCONFUSED*100/total,2)
+    pANGRY=round(cantANGRY*100/total,2)
+    pDISGUSTED=round(cantDISGUSTED*100/total,2)
+    pSAD=round(cantSAD*100/total,2)
+    pCALM=round(cantCALM*100/total,2)
+    pSURPRISED=round(cantSURPRISED*100/total,2)
+    pFEAR=round(cantFEAR*100/total,2)
+    pHAPPY=round(cantHAPPY*100/total,2)
+    
+    
+    respuestas.append(pCONFUSED) 
+    respuestas.append(pANGRY) 
+    respuestas.append(pDISGUSTED) 
+    respuestas.append(pSAD) 
+    respuestas.append(pCALM) 
+    respuestas.append(pSURPRISED) 
+    respuestas.append(pFEAR) 
+    respuestas.append(pHAPPY) 
+    
+    return respuestas
+
+    
+def GetUserForToken(token):    
     resBd=[]
     try:
+        #CONECCION A MYSQL
+        db = mysql.connect(**configBD)
         cursor = db.cursor()
-        query = "select * from users u where u.token="+str(token)
+        query = "select * from Users u where u.token="+str(token)
         cursor.execute(query)
         resBd=cursor.fetchall()         
         if len(resBd)==1: 
@@ -170,18 +279,13 @@ def GetUserForToken(token):
     
     return resBd
 
-def GetUserForDni(dni):
-    #CONECCION A MYSQL
-    db = mysql.connect(
-        host = "db-proyecto-mysql.cluster-cbwzye3glmch.us-east-1.rds.amazonaws.com",
-        user = "sa",
-        passwd = "luismiguel",
-        database = "educati"
-        )
+def GetUserForDni(dni):    
     resBd=None
     try:
+        #CONECCION A MYSQL
+        db = mysql.connect(**configBD)
         cursor = db.cursor()
-        query = "select * from users u where u.dni="+dni
+        query = "select * from Users u where u.dni="+str(dni)
         cursor.execute(query)
         resBd=cursor.fetchall()         
         
@@ -270,6 +374,40 @@ def Cargardata():
             'body': response        
         }      
 
+@app.route("/registrardataset",methods=['POST'])
+def RegistroDataset():    
+    if request.method=='POST':
+        f=request.files['archivo']        
+        EDAD=int(request.form['EDAD'])
+        NOMBRE=str(request.form['NOMBRE'])
+        CURSO=str(request.form['CURSO'])
+        SEXO=str(request.form['SEXO'])
+        NOTA=int(request.form['NOTA'])
+        RESULTADO=str(request.form['RESULTADO'])
+        GRADO=int(request.form['GRADO'])
+        token=request.headers.get('token')
+        # VALIDAR TOKEN
+        if ValidarToken(token)==False:
+            return {
+                'statusCode': 400,
+                'body': "Error de Autenticación"
+                } 
+        res=RegistarDataset(f)
+        
+        #CONECCION A MYSQL
+        db = mysql.connect(**configBD)
+        cursor = db.cursor()                
+        sql="insert into DATASET(CONFUSED,ANGRY,DISGUSTED,SAD,CALM,SURPRISED,FEAR,HAPPY,EDAD,NOMBRE,CURSO,SEXO,NOTA,RESULTADO,GRADO) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        datos=(res[0],res[1],res[2],res[3],res[4],res[5],res[6],res[7],EDAD,NOMBRE,CURSO,SEXO,NOTA,RESULTADO,GRADO)    
+        cursor.execute(sql, datos)   
+        db.commit()
+        db.close()    
+        
+        #MOSTRAR LA RESPUESTA
+        return {
+            'statusCode': 200,
+            'body':  res       
+        }      
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8080,debug=True)
